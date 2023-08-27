85300b6490995294b761bf2eef4279a461dd47ca
hoangvbm2808 <vbmhoang.2808@gmail.com>
2023-08-27 22:57:53 +0700

    Integrate comment facebook

--- src/features/Post/postItem.jsx
+++ src/features/Post/postItem.jsx
@@ -4,6 +4,7 @@ import './post.scss';
 import axiosClient, { endpoints } from '../../api/axiosClient';
 import Container from 'react-bootstrap/Container';
 import { FacebookShareButton, FacebookIcon } from 'react-share';
+import { FacebookProvider, Comments ,Like  } from 'react-facebook';
 
 function PostItem({ postId }) {
     const [post, setPost] = useState(null);
@@ -28,22 +29,31 @@ function PostItem({ postId }) {
         return <div>Loading...{postId}</div>;
 
     }
+    else {
+        
+    }
+
     
 
-   let url = "http://192.168.1.4:3000/post_info/"+id;
+    let url = "http://192.168.1.4:3000/post_info/"+id;
     let appId = 6836544049690538;
+    
 
     return (
         <Container>
             <div > 
                 <h2 className="text-center m-5">{post.title}</h2>
-                <div dangerouslySetInnerHTML={{__html: post.content}}>
-                    
-                </div>
+                <div dangerouslySetInnerHTML={{__html: post.content}} />
                 
             </div>
+            
+            <FacebookProvider appId="6836544049690538" width="100%">
+                <Comments href="https://hoangvbm2808.github.io/webschool/" width="100%" />
+                
+            </FacebookProvider>
+            
             <div className="m-5">
-                <FacebookShareButton url={url} appId={appId} quote={post.title}>
+                <FacebookShareButton url={url} appId={appId}>
                     <FacebookIcon size={50} round/>
                 </FacebookShareButton>
             </div>
